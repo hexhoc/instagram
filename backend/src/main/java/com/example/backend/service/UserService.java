70e57f826cbe7d235029f6d52112d49e8a3c74b6
hexhoc <hexhoc@gmail.com>
2022-05-13 17:32:37 +0300

    Add user exception, service and Dto

---
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,17 @@
+package com.example.backend.service;
+
+import com.example.backend.dto.UserDTO;
+import com.example.backend.entity.User;
+import com.example.backend.payload.request.SignupRequest;
+
+import java.security.Principal;
+
+public interface UserService {
+    User createUser(SignupRequest userIn);
+
+    User updateUser(UserDTO userDTO, Principal principal);
+
+    User getCurrentUser(Principal principal);
+
+    User getUserById(Long id);
+}
