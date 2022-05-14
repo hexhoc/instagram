99246bb50cbfa72c76ccee9652cc5b9f0cf7dc04
hexhoc <hexhoc@gmail.com>
2022-05-14 23:40:57 +0300

    Frontend. Add auth interceptor and token storage service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material-module';
 import { HttpClientModule } from '@angular/common/http';
+import { authInterceptorProviders } from './helper/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
