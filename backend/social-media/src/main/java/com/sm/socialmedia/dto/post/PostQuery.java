353f288c2756de7bb4fff1314107593d28ac8f1a
zhuravskiy_vv <zhuravskiy_vv@nlmk.com>
2023-07-28 19:20:14 +0300

    Update dto description

--- backend/social-media/src/main/java/com/sm/socialmedia/dto/post/PostQuery.java
+++ backend/social-media/src/main/java/com/sm/socialmedia/dto/post/PostQuery.java
@@ -15,22 +15,30 @@ import lombok.Setter;
 @NoArgsConstructor
 @Schema(description = "Post information")
 public class PostQuery {
+
     @Schema(description = "Post id")
     private UUID id;
     private String title;
     private String content;
+
     @Schema(description = "Username of user who created post ")
     private String username;
+
     @Schema(description = "Id of user who created post ")
     private String userId;
+
     @Schema(description = "Group name where post is publish")
     private String groupName;
+
     @Schema(description = "Group id where post is publish")
     private String groupId;
+
     @Schema(description = "Group creator username")
     private String groupCreatorUsername;
+
     @Schema(description = "Group creator id")
     private String groupCreatorId;
+
     @Schema(description = "Number of likes")
     private Integer likes;
     private LocalDateTime createdAt;
